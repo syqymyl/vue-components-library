7058d4a87493bbf16b5538358e48ba24973a0c52
syqsmyl <gemluo@outlook.com>
2023-04-09 06:47:11 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>hi</div>
+  <Aki />
+</template>
+
+<script>
+import Aki from "./components/Aki.vue";
+
+export default {
+  name: "App",
+  components: {
+    // 以下代码等价于 HelloWorld:HelloWorld，左边是标签化名，右边是组件的引用
+    // HelloWorld,
+    Aki,
+  },
+};
+</script>
