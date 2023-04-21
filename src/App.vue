254c7b3d4de50b345ec850ca4d0757547eaee9bb
syqsmyl <gemluo@outlook.com>
2023-04-21 14:29:18 +0800

    topnav 样式更新

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
 <template>
-  <div>
-    导航栏 | <router-link to="/">Aki</router-link> |
-    <router-link to="/xxx">Aki2</router-link>
-    <!-- 添加跳转链接 -->
-  </div>
-  <hr />
   <router-view />
   <!-- 在此处展示组件 -->
 </template>
 
 <script>
 export default {
-  name: "App",
-};
+  name: 'App',
+}
 </script>
