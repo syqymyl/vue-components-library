36dd9b9313297d582626d2301d511bfb3c557f0c
syqsmyl <gemluo@outlook.com>
2023-04-21 16:17:38 +0800

    Home 页面的 css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,8 @@
   <div>
     <Topnav />
     <div class="banner">
-      <h1>Vue UI</h1>
-      <h2>一个厉害的 UI 框架</h2>
+      <h1>山竹 UI</h1>
+      <h2>一个基于 Vue 的 UI 组件库</h2>
       <p class="actions">
         <a href="">Github</a>
         <a href="">开始</a>
@@ -19,4 +19,26 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
+</style>
