7e067df353bd02d5b7e6ed6f218ac5f4868fa809
syqsmyl <gemluo@outlook.com>
2023-04-23 09:42:31 +0800

    Switch css 2

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -5,7 +5,7 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { ref } from 'vue'
 import Switch from '../lib/Switch.vue'
 export default {
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -8,7 +8,7 @@ export default {
   props: { value: Boolean }, // 接收外部传过来的变量
   setup(props, context) {
     const toggle = () => {
-      context.emit('update:value', !props.value) 
+      context.emit('update:value', !props.value)
     }
     return { toggle }
   },
@@ -22,24 +22,38 @@ button {
   height: $h;
   width: $h * 2;
   border: none;
-  background: grey;
+  background: #bfbfbf;
   border-radius: $h/2;
   position: relative;
-}
-span {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background: white;
-  border-radius: $h2/2;
-  transition: left 250ms;
-}
-button.checked > span {
-  left: calc(100% - #{$h2} - 2px);
-}
-button.checked {
-  background: blue;
+  > span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: all 250ms;
+  }
+  &.checked {
+    background: #6134c2;
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+  &:focus {
+    outline: none;
+  }
+  &:active {
+    > span {
+      width: $h2 + 4px;
+    }
+  }
+  &.checked:active {
+    > span {
+      width: $h2 + 4px;
+      margin-left: -4px;
+    }
+  }
 }
 </style>
