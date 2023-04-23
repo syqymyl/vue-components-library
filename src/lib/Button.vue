e328bce2f1cedf2848005e188db160902381be4f
syqsmyl <gemluo@outlook.com>
2023-04-23 14:57:07 +0800

    基础样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -12,7 +12,40 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-div {
-  border: 1px solid red;
+$h: 32px; // 提前声明变量
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+.mango-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+  & + & {
+    // mango-button + mango-button
+    margin-left: 8px;
+  }
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-moz-focus-inner {
+    // 兼容 firefox
+    border: 0;
+  }
 }
 </style>
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -36,7 +36,7 @@ button {
     transition: all 250ms;
   }
   &.checked {
-    background: #6134c2;
+    background: #1890ff;
     > span {
       left: calc(100% - #{$h2} - 2px);
     }
