4ff95fc01fc92a37dcd66e27fd445ce5af7c7749
syqsmyl <gemluo@outlook.com>
2023-04-24 16:48:32 +0800

    背景渐变

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -25,7 +25,6 @@ export default {
 
 <style lang="scss" scoped>
 .topnav {
-  background: pink;
   display: flex;
   padding: 16px;
   position: fixed;
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -133,7 +133,7 @@ $border-color: #d9d9d9;
       left: 0;
       bottom: -1px;
       width: 100px;
-      transition: all 250ms;
+      transition: all 250ms; // 滑动动画
     }
   }
 
