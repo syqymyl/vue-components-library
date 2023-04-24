52d27d6d7ba1c720a418a2da552d078204b57b9a
syqsmyl <gemluo@outlook.com>
2023-04-24 12:03:09 +0800

    Tabs css 1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>
-    <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
-    <component v-for="(c, index) in defaults" :is="c" :key="index" />
+  <div class="mango-tabs">
+    <div class="mango-tabs-nav">
+      <!-- 渲染：t 为 title, c 为 组件 Tab -->
+      <div
+        class="mango-tabs-nav-item"
+        v-for="(t, index) in titles"
+        :key="index"
+      >
+        {{ t }}
+      </div>
+    </div>
+    <div class="mango-tabs-content">
+      <component
+        class="mango-tabs-content-item"
+        v-for="(c, index) in defaults"
+        :is="c"
+        :key="index"
+      />
+    </div>
   </div>
 </template>
 
@@ -9,7 +25,7 @@
 import Tab from './Tab.vue'
 export default {
   setup(props, context) {
-    const defaults = context.slots.default() // 获取外面传过来的子内容
+    const defaults = context.slots.default() // 获取外面传过来的子内容(两个 Tab 对象)
     // 返回 true，说明 defaults[0] 的类型正好是 Tab
     // console.log(defaults[0].type === Tab)
     defaults.forEach((tag) => {
@@ -27,3 +43,35 @@ export default {
   },
 }
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+
+.mango-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+
+      &:first-child {
+        margin-left: 0;
+      }
+
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
