2f516ab13ea2a5ac81bf834f393ebebf6c7ee2d4
syqsmyl <gemluo@outlook.com>
2023-04-26 06:01:51 +0800

    优化 Demo

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,21 +1,17 @@
 <template>
   <div class="demo">
+    <!-- 展示标题 -->
     <h2>{{ component.__sourceCodeTitle }}</h2>
     <div class="demo-component">
       <!-- 引用组件 -->
       <component :is="component" />
     </div>
     <div class="demo-actions">
-      <Button>查看代码</Button>
+      <Button @click="toggleCode">查看代码</Button>
     </div>
-    <div class="demo-code">
-      <!-- 使用 Prism 对源码进行高亮，component.__sourceCode 是文本类型的源码 -->
-      <pre
-        class="language-html"
-        v-html="
-          Prism.highlight(component.__sourceCode, Prism.languages.html, 'html')
-        "
-      />
+    <div class="demo-code" v-if="codeVisible">
+      <!-- 代码展示 -->
+      <pre class="language-html" v-html="html" />
     </div>
   </div>
 </template>
@@ -25,15 +21,37 @@ import Button from "../lib/Button.vue";
 // 使用 Prism 对代码进行高亮
 import "prismjs";
 import "prismjs/themes/prism.css";
+import { computed, ref } from "vue";
 const Prism = (window as any).Prism;
+
 export default {
+  components: {
+    Button,
+  },
   props: {
     // 传入 SwitchDemo1 和 SwitchDemo2
     component: Object,
   },
-  setup() {
+  setup(props) {
+    // 使用  Prism 对代码进行高亮
+    const html = computed(() => {
+      return Prism.highlight(
+        props.component.__sourceCode,
+        Prism.languages.html,
+        "html"
+      );
+    });
+    // 点击事件：是否显示代码
+    const toggleCode = () => {
+      codeVisible.value = !codeVisible.value;
+    };
+    // 默认不显示
+    const codeVisible = ref(false);
     return {
       Prism,
+      html,
+      codeVisible,
+      toggleCode,
     };
   },
 };
