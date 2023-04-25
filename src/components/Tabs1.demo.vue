04766223a10d6709551a796abfa52f5f59cead3a
syqsmyl <gemluo@outlook.com>
2023-04-26 07:17:53 +0800

    改造 Tabs

---
+++ src/components/Tabs1.demo.vue
@@ -0,0 +1,23 @@
+<demo>
+常规使用
+</demo>
+<template>
+  <!-- 通过 Tabs 控制导航的选中和内容的切换，selected 用 title 表示 -->
+  <Tabs v-model:selected="x">
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航22222">内容2</Tab>
+  </Tabs>
+</template>
+
+<script lang="ts">
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
+import { ref } from 'vue'
+export default {
+  components: { Tabs, Tab },
+  setup() {
+    const x = ref('导航1')
+    return { x }
+  },
+}
+</script>
