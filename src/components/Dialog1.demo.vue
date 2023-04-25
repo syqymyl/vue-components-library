ed2f54a6e291e48c34753330e1717483f0f766f4
syqsmyl <gemluo@outlook.com>
2023-04-26 07:02:12 +0800

    改造 Dialog

---
+++ src/components/Dialog1.demo.vue
@@ -0,0 +1,50 @@
+<demo>
+常规使用
+</demo>
+<template>
+  <div>
+    <Button @click="toggle">打开对话框</Button>
+    <Dialog
+      v-model:visible="x"
+      :closeOnClickOverlay="false"
+      :ok="f1"
+      :cancel="f2"
+    >
+      <template v-slot:content>
+        <div>用户名：</div>
+        <div>密码：</div>
+      </template>
+      <template v-slot:title>
+        <strong>登录</strong>
+      </template>
+    </Dialog>
+  </div>
+</template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import { ref } from 'vue'
+export default {
+  components: {
+    Dialog,
+    Button,
+  },
+  setup() {
+    const x = ref(false)
+    const toggle = () => {
+      x.value = !x.value
+    }
+    const f1 = () => {
+      return false
+    }
+    const f2 = () => {}
+    return {
+      x,
+      toggle,
+      f1,
+      f2,
+    }
+  },
+}
+</script>
--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,49 +1,18 @@
 <template>
-  <div>Dialog 示例</div>
-  <h1>示例1</h1>
-  <!-- 点击按钮弹出或关闭弹窗 -->
-  <Button @click="toggle">toggle</Button>
-  <Dialog
-    v-model:visible="x"
-    :closeOnClickOverlay="false"
-    :ok="f1"
-    :cancel="f2"
-  >
-    <!-- 使用具名插槽控制弹窗内容 -->
-    <template v-slot:content>
-      <div>用户名：</div>
-      <div>密码：</div>
-    </template>
-    <template v-slot:title>
-      <strong>登录</strong>
-    </template></Dialog
-  >
+  <h1>Dialog 示例</h1>
+  <Demo :component="Dialog1Demo" />
 </template>
 
 <script lang="ts">
-import Dialog from '../lib/Dialog.vue'
-import Button from '../lib/Button.vue'
-import { ref } from 'vue'
+import Demo from './Demo.vue'
+import Dialog1Demo from './Dialog1.demo.vue'
 export default {
   components: {
-    Dialog,
-    Button,
+    Demo,
   },
   setup() {
-    const x = ref(false)
-    const toggle = () => {
-      x.value = !x.value
-    }
-    // 当返回 true 时，点击 ok 即关闭弹窗；当返回false 时，无法关闭弹窗（写填满文字才能关闭弹窗的功能）
-    const f1 = () => {
-      return false
-    }
-    const f2 = () => {}
     return {
-      x,
-      toggle,
-      f1,
-      f2,
+      Dialog1Demo,
     }
   },
 }
