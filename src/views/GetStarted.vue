90a874558c1565c306efde84bd16046d3b89ce17
syqsmyl <gemluo@outlook.com>
2023-04-27 00:36:25 +0800

    完善文档

--- src/views/GetStarted.vue
+++ src/views/GetStarted.vue
@@ -4,7 +4,8 @@
 
     <p>请先<a href="#/doc/install">安装</a>本组件库。</p>
     <p>然后在你的代码中写入下面的代码</p>
-    <pre><code>import { Button, Tab, Tabs, Switch, Dialog, openDialog } from "shanzhu-ui"</code></pre>
+    <pre><code>import { Button, Tab, Tabs, Switch, Dialog, openDialog } from "shanzhu-ui"
+import "shanzhu-ui/dist/lib/shanzhu.css"</code></pre>
     就可以使用我提供的组件了。
 
     <h2>Vue 单文件组件</h2>
@@ -14,10 +15,15 @@
 &lt;/template&gt;
 &lt;script&gt;
   import { Button, Tab, Tabs, Switch, Dialog, openDialog } from "shanzhu-ui"
+  import "shanzhu-ui/dist/lib/shanzhu.css"
   export default {
     components: {Button}
   }
 &lt;/script&gt;
  </code></pre>
+
+    <h2>注意事项</h2>
+    <p>如果你想使用组件中的示例代码，使用时请将示例代码中的这句代码删除：</p>
+    <pre><code>import { * } from '../lib/index'</code></pre>
   </article>
 </template>
