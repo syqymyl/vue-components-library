2fbeab303d8a0ad49d378f30e1558e3f9c322e08
syqsmyl <gemluo@outlook.com>
2023-05-18 15:57:13 +0800

    消除 markdown 重复

--- src/router.ts
+++ src/router.ts
@@ -7,11 +7,16 @@ import ButtonDemo from './components/ButtonDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 
+import { h } from 'vue'
+import Markdown from './components/Markdown.vue'
 // 添加“介绍”“快速入手”“安装”的路由
-import Intro from './views/Intro.vue'
-import GetStarted from './views/GetStarted.vue'
-import Install from './views/Install.vue'
+import intro from './markdown/intro.md'
+import getStarted from './markdown/getStarted.md'
+import install from './markdown/install.md'
+
 const history = createWebHashHistory()
+const md = (string) => h(Markdown, { content: string, key: string })
+
 export const router = createRouter({
   history: history,
   routes: [
@@ -21,9 +26,9 @@ export const router = createRouter({
       component: Doc,
       children: [
         { path: '', redirect: 'doc/intro' },
-        { path: 'intro', component: Intro },
-        { path: 'get-started', component: GetStarted },
-        { path: 'install', component: Install },
+        { path: 'intro', component: md(intro) },
+        { path: 'get-started', component: md(getStarted) },
+        { path: 'install', component: md(install) },
         { path: 'switch', component: SwitchDemo },
         { path: 'button', component: ButtonDemo },
         { path: 'dialog', component: DialogDemo },
